697869df04b67dfaa2ed7b30ccec90908ec8dd6b
AbdulArfaz <abdularfaz00@gmail.com>
2026-09-14 23:59:27 +0530

    cleaning bugs

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -20,7 +20,7 @@ const userSchema = new Schema(
     role: {
       type: String,
       enum: ["owner", "user"],
-      default: "user",
+      default: "owner",
     },
     image: {
       type: String,
