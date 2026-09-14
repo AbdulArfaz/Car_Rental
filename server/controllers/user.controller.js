3df56a9b966514f99607b86a370b9310ef4e2583
AbdulArfaz <abdularfaz00@gmail.com>
2026-09-14 22:07:59 +0530

    fixed bug

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -133,8 +133,8 @@ export const logoutUser = asyncHandler(async (req, res) => {
   );
   const options = {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+    secure: true,
+    sameSite: "none",
   };
   return res
     .status(200)
